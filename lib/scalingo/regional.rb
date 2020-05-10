8b1928c033b097b2c0acdd753b2082c55dbc8dfd
Kevin Soltysiak <k@ksol.fr>
2020-05-10 19:35:06 +0200

    Collaborators

--- lib/scalingo/regional.rb
+++ lib/scalingo/regional.rb
@@ -4,11 +4,13 @@ module Scalingo
   class Regional < API::BaseClient
     require "scalingo/regional/addons"
     require "scalingo/regional/apps"
+    require "scalingo/regional/collaborators"
     require "scalingo/regional/container_sizes"
 
     register_handlers!(
       addons: Addons,
       apps: Apps,
+      collaborators: Collaborators,
       container_sizes: ContainerSizes,
     )
   end
---
+++ lib/scalingo/regional/collaborators.rb
@@ -0,0 +1,35 @@
+require "scalingo/api/endpoint"
+
+module Scalingo
+  class Regional::Collaborators < API::Endpoint
+    def for(app_id)
+      response = connection.get("apps/#{app_id}/collaborators")
+
+      unpack(response, key: :collaborators)
+    end
+
+    def destroy(app_id, collaborator_id)
+      response = connection.delete("apps/#{app_id}/collaborators/#{collaborator_id}")
+
+      unpack(response)
+    end
+
+    def invite(app_id, email)
+      data = {
+        collaborator: {
+          email: email,
+        },
+      }
+
+      response = connection.post("apps/#{app_id}/collaborators", data)
+
+      unpack(response, key: :collaborator)
+    end
+
+    def accept(token)
+      response = connection.get("apps/collaboration", { token: token })
+
+      unpack(response)
+    end
+  end
+end
