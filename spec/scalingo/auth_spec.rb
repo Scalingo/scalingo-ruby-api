b6dbb51f538fe038c3ea88c6595836462134554d
Kevin Soltysiak <kevin@scalingo.com>
2020-06-13 16:03:59 +0200

    Specs: auth/regional main clients

---
+++ spec/scalingo/auth_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+RSpec.describe Scalingo::Auth do
+  subject { described_class.new(:client, :url) }
+
+  %w[keys scm_integrations tokens two_factor_auth user].each do |section|
+    it "handles requests for #{section}" do
+      expect(subject.respond_to?(section)).to be true
+    end
+  end
+
+  it "aliases tfa" do
+    expect(subject.tfa).to eq(subject.two_factor_auth)
+  end
+end
---
+++ spec/scalingo/regional_spec.rb
@@ -0,0 +1,14 @@
+require "spec_helper"
+
+RSpec.describe Scalingo::Regional do
+  subject { described_class.new(:client, :url) }
+
+  %w[
+    addons apps collaborators containers deployments domains environment events
+    logs metrics notifiers operations scm_repo_links
+  ].each do |section|
+    it "handles requests for #{section}" do
+      expect(subject.respond_to?(section)).to be true
+    end
+  end
+end
