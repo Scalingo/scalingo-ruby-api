d6b93da24d15529824e5d6d994eb3881377fe30b
aurelien-reeves-scalingo <aurelien.reeves@scalingo.com>
2022-12-20 17:33:51 +0100

    Fix aliases

--- lib/scalingo/client.rb
+++ lib/scalingo/client.rb
@@ -27,7 +27,7 @@ module Scalingo
         scalingo: self,
       )
     end
-    alias osc_fr1 apps_api_osc_fr1
+    alias apps_api_osc_fr1 osc_fr1
 
     def osc_secnum_fr1
       @osc_secnum_fr1 ||= Regional.new(
@@ -35,7 +35,7 @@ module Scalingo
         scalingo: self,
       )
     end
-    alias osc_secnum_fr1 apps_api_osc_secnum_fr1
+    alias apps_api_osc_secnum_fr1 osc_secnum_fr1
 
     def db_api_osc_fr1
       @db_api_osc_fr1 ||= RegionalDatabase.new(
