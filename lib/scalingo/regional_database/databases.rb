156c309abbb580eadaa2381735535d3a416f6632
Kevin Soltysiak <kevin@scalingo.com>
2024-02-12 00:27:47 +0100

    refactor: migrate db api endpoints and specs

--- lib/scalingo/regional_database/databases.rb
+++ lib/scalingo/regional_database/databases.rb
@@ -2,26 +2,7 @@ require "scalingo/api/endpoint"
 
 module Scalingo
   class RegionalDatabase::Databases < API::Endpoint
-    def find(id, headers = nil, &block)
-      data = nil
-
-      database_connection(id).get(
-        "databases/#{id}",
-        data,
-        headers,
-        &block
-      )
-    end
-
-    def upgrade(id, headers = nil, &block)
-      data = nil
-
-      database_connection(id).post(
-        "databases/#{id}/upgrade",
-        data,
-        headers,
-        &block
-      )
-    end
+    get :find, "databases/{id}"
+    post :upgrade, "databases/{id}/upgrade"
   end
 end
