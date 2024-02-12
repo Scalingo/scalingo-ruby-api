63a26052e5a8982e0d84f4fbbf7c91b7c0c3f9e9
Kevin Soltysiak <kevin@scalingo.com>
2024-02-12 10:16:24 +0100

    cleaner code, changelog entries

--- lib/scalingo/faraday/extract_meta.rb
+++ lib/scalingo/faraday/extract_meta.rb
@@ -3,23 +3,29 @@ require "faraday"
 module Scalingo
   class ExtractMeta < Faraday::Middleware
     def on_complete(env)
-      # Only hash-like objects are relevant
       return unless env.body.is_a?(Hash)
 
-      # Extract meta from response
-      if env.body[:meta]
-        env[:response_meta] = env.body.delete(:meta)
-      end
+      extract_metadata(env)
+      extract_cursor_metadata(env)
+    end
 
-      # Extract cursor-based pagination
-      if env.body.key?(:next_cursor)
-        env[:response_meta] = {
-          cursor_pagination: {
-            next_cursor: env.body.delete(:next_cursor),
-            has_more: env.body.delete(:has_more)
-          }.compact
-        }
-      end
+    private
+
+    def extract_metadata(env)
+      return unless env.body[:meta]
+
+      env[:response_meta] = env.body.delete(:meta)
+    end
+
+    def extract_cursor_metadata(env)
+      return unless env.body.key?(:next_cursor)
+
+      env[:response_meta] = {
+        cursor_pagination: {
+          next_cursor: env.body.delete(:next_cursor),
+          has_more: env.body.delete(:has_more)
+        }.compact
+      }
     end
   end
 end
